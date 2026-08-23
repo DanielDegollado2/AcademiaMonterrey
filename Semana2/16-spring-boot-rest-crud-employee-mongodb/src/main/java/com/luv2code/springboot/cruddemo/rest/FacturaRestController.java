50f33f9a8f08889c909b220b6624ee608274e70d
DanielDegollado2 <danieldegrdz@gmail.com>
2026-08-22 23:27:13 -0600

    Semana2 - Commit 10: Fix FacturaRestController, proyecto Spring Data Mongodb

--- Semana2/16-spring-boot-rest-crud-employee-mongodb/src/main/java/com/luv2code/springboot/cruddemo/rest/FacturaRestController.java
+++ Semana2/16-spring-boot-rest-crud-employee-mongodb/src/main/java/com/luv2code/springboot/cruddemo/rest/FacturaRestController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springboot.cruddemo.entity.Factura;
 import com.luv2code.springboot.cruddemo.service.FacturaService;
 
+@RestController
+@RequestMapping("/api")
 public class FacturaRestController {
 	private FacturaService facturaService;
 
